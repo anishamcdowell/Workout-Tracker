aec58f4e9e9bf881d810b00a80cd8a6f1a712978
Anisha McDowell <anishamcdowell@gmail.com>
2021-05-16 12:23:00 -0400

    server file made

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const routes = require("./controller");
+const connection = require("./config/connection.js");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+connection.on("connected", () => {
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`)
+    });
+});
